ce4661d1e6dbb7c741a8fbcd5b831a82668e20e9
ELena Tabakov <elena.tabakov@gmail.com>
2026-01-12 18:54:17 +0200

    Git pages

--- ai-prompt-manager/vite.config.ts
+++ ai-prompt-manager/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ai-prompt-manager/', // ← замени на название твоего репозитория
-})
+  base: '/AI-Prompt-Manager/',
+});
